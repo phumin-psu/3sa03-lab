90b09b89dc179e1677006546b116fa1e03fae5e5
phumin-psu <6210110272@psu.ac.th>
2021-07-09 00:16:01 +0700

    first hello world

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
 
 function App() {
   return (
     <div>
-      <CharacterCard/>
+      Hello World
     </div>
   );
 }
